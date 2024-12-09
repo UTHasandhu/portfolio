bb4ec6e1ac3929af8041826a5e1fdc20c8719bd8
UTHasandhu <hasandhu@utexas.edu>
2024-12-08 22:48:40 -0600

    Testing a change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          So did this work.
+          I want my changes to track
         </p>
         <a
           className="App-link"
