418a4a8c0e727d07bfb086c9c6102e6ff06587f3
UTHasandhu <hasandhu@utexas.edu>
2024-12-18 07:19:54 -0600

    Fix header to floor

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,22 +3,42 @@ import corvette from "../Media/c5_logo_decal.jpg";
 
 function Header() {
   return (
-    <div class="container-xxl">
-      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-current="page">
-          <div class="p-2 g-col-6">
+    <div className="container-xxl">
+      <header className="d-flex flex-wrap justify-content-between align-items-end py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          className="d-flex align-items-end mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+          aria-current="page"
+        >
+          <div className="p-2 g-col-6">
             <img className="homeimg" src={corvette} />
           </div>
-          <div class="p-2 g-col-6">
+          <div className="p-2 g-col-6">
             <h4 className="hometxt">HARMAN SANDHU</h4>
           </div>
         </a>
 
-        <ul class="nav nav-pills">
-          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Education & Experience</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Projects</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <ul className="nav nav-pills align-items-end">
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Education & Experience
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              About
+            </a>
+          </li>
         </ul>
       </header>
     </div>
